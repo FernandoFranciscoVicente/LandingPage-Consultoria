a8296fd1f5cc13c5db911ed95d12144f70ecaf52
Fernando VI. <f3rnandogaff@gmail.com>
2025-10-05 15:37:33 -0600

    Primera y Segunda seccion

---
+++ script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper",{
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate:15,
+        stretch:0,
+        depth:300,
+        modifier:1,
+        slideShadows:true,
+    },
+    loop:true,
+    autoplay: {delay: 2500, disableOnInteraction: false},
+    navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+    pagination: { el: ".swiper-pagination", clickable: true },
+    observer: true,
+    observeParents: true
+});
+
